e7469f2d8c603c4d66178da65f7db77fc9f0efe1
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2025-02-24 19:52:06 +0800

    Add files via upload

--- components/Dev.tsx
+++ components/Dev.tsx
@@ -20,7 +20,7 @@ const Dev = () => {
       <section className="py-16 px-4">
           <div className="container mx-auto text-center">
             <h2 className="text-3xl font-bold mb-8 text-white">Developer</h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center justify-center">
               <div
                 key="_"
                 className="bg-gray-800 bg-opacity-50 p-6 rounded-lg transition-all duration-300 ease-in-out transform hover:scale-105 hover:border-blue-500 border-2 border-transparent flex flex-col items-center"
