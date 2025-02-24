78d0e1759a3f8647cd3dbb66c2dac21a4d9192d3
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2025-02-24 21:01:04 +0800

    Add files via upload

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -62,13 +62,15 @@ const Navbar = () => {
             <Link
               key={item.name}
               href={item.href}
-              className={`flex items-center gap-2 md:inline-block md:ml-6 py-2 px-4 transition-all duration-300 ease-in-out ${
+              className="flex flex-row items-center md:inline-flex md:ml-6 py-2 px-4 transition-all duration-300 ease-in-out ${
                 isOpen ? "opacity-100 translate-y-0" : "opacity-0 md:opacity-100 translate-y-full md:translate-y-0"
               } ${isOpen ? `delay-[${index * 100}ms]` : ""}`}
               style={{ transitionDelay: isOpen ? `${index * 100}ms` : "0ms" }}
             >
-              <span className="w-5 h-5 flex-shrink-0">{item.icon}</span>
-              <span className="whitespace-nowrap">{item.name}</span>
+              <span className="flex items-center space-x-2">
+                {item.icon}
+                <span className="inline-block">{item.name}</span>
+              </span>
             </Link>
           ))}
         </div>
