228360be90458d6a1fe816eb52117f8b6f2c49d1
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2025-03-21 12:56:43 +0800

    Add files via upload

--- components/WhitelistCheckpoint.tsx
+++ components/WhitelistCheckpoint.tsx
@@ -121,9 +121,11 @@ export default function Checkpoint() {
         }
     }
 
+    const web = process.env.WEB
+
     const authentication = async () => {
       try {
-        const response = await axios.post("http://127.0.0.1:3000/v1/whitelist?i=" + hexEncode(Math.floor(Date.now() / 1000) + " " + hwid + " " + unix))
+        const response = await axios.post(web + "/v1/whitelist?i=" + hexEncode(Math.floor(Date.now() / 1000) + " " + hwid + " " + unix))
         if (response.data.status == 200) {
           localstorage.removeItem("hdocnoOe")
           window.location.href = "/whitelist/checkpoint"
@@ -134,7 +136,7 @@ export default function Checkpoint() {
     };
 
     const main = async () => {
-      const response = await axios.get("http://127.0.0.1:3000/v1/whitelist?d=" + hwid)
+      const response = await axios.get(web + "/v1/whitelist?d=" + hwid)
       if (response.data.valid) {
         setButton(false)
         setCaptcha(false)
