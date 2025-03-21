3de1fe9fea389438b534a9e5a195ca402182f01d
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2025-03-21 14:16:36 +0800

    Add files via upload

--- app/api/checkwhitelist/route.js
+++ app/api/checkwhitelist/route.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 export default async function handler(req, res) {
   try {
     const response = await axios.get("https://api.overdrivehub.xyz/v1/whitelist?d=" + req.query.d);
-    res.status(200).json(response.data);
+    res.status(200).json(response.data || {
+      status: 400,
+      message: "Something went wrong on our end."
+    });
   } catch (error) {
     console.error("Proxy API Error:", error);
     res.status(500).json({ error: "Failed to fetch data" });
