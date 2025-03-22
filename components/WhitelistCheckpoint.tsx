7dc40150c41446de73d203a94ae74972584326d8
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2025-03-22 19:33:45 +0800

    Add files via upload

--- components/WhitelistCheckpoint.tsx
+++ components/WhitelistCheckpoint.tsx
@@ -125,7 +125,7 @@ export default function Checkpoint() {
 
     const authentication = async () => {
       try {
-        const response = await axios.post("https://corsproxy.io/?url=https://api.overdrivehub.xyz/v1/whitelist/" + hexEncode(Math.floor(Date.now() / 1000) + " " + hwid + " " + unix))
+        const response = await axios.post("https://api.overdrivehub.xyz/v1/whitelist/" + hexEncode(Math.floor(Date.now() / 1000) + " " + hwid + " " + unix))
         if (response.data.status == 200) {
           localstorage.removeItem("hdocnoOe")
           window.location.href = "/whitelist/checkpoint"
@@ -136,7 +136,7 @@ export default function Checkpoint() {
     };
 
     const main = async () => {
-      const response = await axios.get("https://corsproxy.io/?url=https://api.overdrivehub.xyz/v1/whitelist/" + hwid)
+      const response = await axios.get("https://api.overdrivehub.xyz/v1/whitelist/" + hwid)
       if (response.data.valid) {
         setButton(false)
         setCaptchaIcon(false)
