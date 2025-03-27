a522542a8b03cf45f49a22cbb5f1d4900dda27a4
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2025-03-27 12:42:39 +0800

    Add files via upload

--- app/tos/page.tsx
+++ app/tos/page.tsx
@@ -9,35 +9,31 @@ const TermsOfService = () => {
         <section className="mb-6">
           <h2 className="text-2xl font-semibold mb-3">Introduction</h2>
           <p>
-            Welcome to Overdrive H ("Overdrive H," "we," "us," or "our"). We are a limited liability corporation, and
-            these Terms of Service ("Terms") govern your access to and use of our free software, Overdrive H (the
-            "Software"). By accessing, or using the Software, you ("User" or "you") agree to be bound by these Terms. If
-            you do not agree to these Terms, you must not use the Software.
+            Welcome to Overdrive H ("Overdrive H," "we," "us," or "our"). We are a limited liability corporation, and these Terms of Service ("Terms") govern your access to and use of our free software, Overdrive H (the "Software"). By accessing or using the Software, you ("User" or "you") agree to comply with these Terms. If you do not agree to these Terms, you must refrain from using the Software.
           </p>
         </section>
 
         <section className="mb-6">
-          <h2 className="text-2xl font-semibold mb-3">Is Overdrive H logging users?</h2>
-          <p>No, we do not log users.</p>
+          <h2 className="text-2xl font-semibold mb-3">Does Overdrive H log user activity?</h2>
+          <p>We do not maintain user records; however, we store your hardware ID upon completing the key system. Rest assured, we do not share this information with any third parties.</p>
         </section>
 
         <section className="mb-6">
           <h2 className="text-2xl font-semibold mb-3">Can we still get a refund from the Premium/Exclusive gamepass?</h2>
-          <p>No, we don't do a refund.</p>
+          <p>No, we do not offer refunds.</p>
         </section>
 
         <section className="mb-6">
           <h2 className="text-2xl font-semibold mb-3">
-            Will I get banned once I cracked Overdrive H Premium or Exclusive?
+            Will I be banned if I bypass Overdrive H Premium or Exclusive?
           </h2>
-          <p>Yes, we will completely ban your account once you crack one of our products.</p>
+          <p>Yes, your account will be permanently banned if you attempt to bypass any of our products.</p>
         </section>
 
         <section className="mb-6">
           <h2 className="text-2xl font-semibold mb-3">User's Liability</h2>
           <p>
-            By using Overdrive H, you agree that you are responsible for any damages, costs,
-            or legal issues that arise from your actions or from any misuse of the Software.
+            By using Overdrive H, you acknowledge that you are solely responsible for any damages, expenses, or legal consequences resulting from your actions or any misuse of the software.
           </p>
         </section>
 
